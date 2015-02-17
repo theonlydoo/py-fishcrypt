e1ce5a320d1026b087d1cac9f249c50da2fa3a14
Florian Krause <fladd@web.de>
2015-02-17 23:53:47 +0100

    Update fishcrypt.py

--- fishcrypt.py
+++ fishcrypt.py
@@ -19,7 +19,7 @@
 #   * 5.00
 #      + Fixed compatibility for networks with identify-msg (e.g. freenode) in key exchange (fladd)
 #      + Added FiSHLiM unload (fladd)
-#      + Fixed /ME+ compatibility to FiSHLiM (fladd)
+#      + Changed /ME+ to be compatibile with FiSHLiM (fladd)
 #
 #   * 4.21
 #      + Fixed Empty Action /me
